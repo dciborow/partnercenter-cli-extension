5beadc55bee28e2f0aabfefd090faa58b5c74513
Daniel Ciborowski <dciborow@microsoft.com>
2023-02-25 23:21:41 -0500

    Update rest.py

--- partnercenter/azext_partnercenter/vendored_sdks/v1/partnercenter/rest.py
+++ partnercenter/azext_partnercenter/vendored_sdks/v1/partnercenter/rest.py
@@ -89,7 +89,7 @@ class RESTClientObject(object):
                 key_file=configuration.key_file,
                 proxy_url=configuration.proxy,
                 proxy_headers=configuration.proxy_headers,
-                **addition_pool_args
+                **addition_pool_args,
             )
         else:
             self.pool_manager = urllib3.PoolManager(
